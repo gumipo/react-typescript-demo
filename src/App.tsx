8d166f23dfe08692bb915ca67309d607920598d0
gumipo <10sei0otto.22@gmail.com>
2021-01-09 15:36:31 +0900

    環境構築

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Layout } from "./components/layouts";
+
+const App = () => {
+  return <Layout>foober</Layout>;
+};
+
+export default App;
---
+++ src/components/atoms/TextInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "../../styles/atoms/form.module.scss";
+
+const TextInput: React.FC = () => {
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  );
+};
+
+export default TextInput;
